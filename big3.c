50396481fcd796ada79e01bc9037d0eee29efbcd
reddyprashanth305 <96612820+reddyprashanth305@users.noreply.github.com>
2021-12-31 16:44:44 +0530

    Update big3.c

--- big3.c
+++ big3.c
@@ -15,7 +15,6 @@ biggest3() {
     // if n3 is greater than both n1 and n2, n3 is the largest
     if (n3 >= n1 && n3 >= n2)
         printf("%.2f is the largest number.", n3);
-        printf("\n this is prashanth");
 
     // return 0;
 }
